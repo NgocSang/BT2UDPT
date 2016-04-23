109421e6675b1f4fd2510f2eaca48c949d9913fd
SuongTran0911 <1312498@student.hcmus.edu.vn>
2016-04-23 15:21:24 +0700

    Hộp thư đi

---
+++ SendBox/routes/modules/message.js
@@ -0,0 +1,34 @@
+var crypto 		= require('crypto');
+var MongoDB 	= require('mongodb').Db;
+var Server 		= require('mongodb').Server;
+var moment 		= require('moment');
+var ObjectID = require('mongodb').ObjectID;
+
+
+var dbPort 		= 27017;
+var dbHost 		= 'localhost';
+var dbName 		= 'loginsystem';
+
+var db = new MongoDB(dbName, new Server(dbHost, dbPort, {auto_reconnect: true}), {w: 1});
+	db.open(function(e, d){
+	if (e) {
+		console.log(e);
+	}	else{
+		console.log('connected to database :: ' + dbName);
+	}
+});
+
+var message = db.collection('message');
+
+
+exports.SenderlistMessage = function(email, callback)
+{
+	message.find({sender:email}).sort({date: -1}).toArray(function(e, o) {
+		if (o){
+			callback(null,o);
+		}	else{
+			callback('not message');
+		}
+	});
+}
+
